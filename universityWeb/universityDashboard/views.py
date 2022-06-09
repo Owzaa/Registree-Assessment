359a573079e6609968ad883747df38717bf139da
Owzaa <nyondo.olwethu@gmail.com>
2022-06-09 13:05:08 +0200

    commit on master

--- universityWeb/universityDashboard/views.py
+++ universityWeb/universityDashboard/views.py
@@ -11,13 +11,20 @@ URL__API = ['https://registree-coding-challenge.glitch.me/UJ/marks',
 
 
 # GET__API then return University to JSON() DATA
-def dashView(request,URL__API):
+def dashView(request):
     title = 'University - Dashboard'
-    response = requests.get(URL__API.json())
-    return render(request,'universityDashboard/dashboard.html',{'response': response, 'title':title})
+    url = URL__API
+    response = requests.get(url,headers={'Authorization': 'Bearer %s' 'access_token'})
+    university = response.json()
+    university_marks = []
+    university_names = []
+    for i in range(len(university['university'])):
+        university_marks.append(university['university'][i])
+        university_names.append(university['university'][i])
+    return render(request,'universityDashboard/dashboard.html',{'response': response, 'title':title,'universities':universities})
 
 # Dashboard TableView
 class DashboardTable(TemplateView):
-    Template_name = 'dashboard.html'
+    Template_name = 'universityDashboard/dashboard.html'
     def get_context_data (self,*args, **kwargs):
         pass  
