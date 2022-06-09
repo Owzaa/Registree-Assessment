269302a38cd6389fe930a8341fce52e8acbd3bbc
Owzaa <nyondo.olwethu@gmail.com>
2022-06-09 11:13:23 +0200

    commit on master

--- universityWeb/universityDashboard/views.py
+++ universityWeb/universityDashboard/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+import requests
 
-# Create your views here.
+
+# API LINKS:
+URL__API = ['https://registree-coding-challenge.glitch.me/UJ/marks',
+            'https://registree-coding-challenge.glitch.me/UJ/names',
+            'https://registree-coding-challenge.glitch.me/SU/marks',
+            'https://registree-coding-challenge.glitch.me/su/names']
+
+
+# GET__API then return University to JSON() DATA
+def dashView(request,URL__API):
+    response = requests.get(URL__API.json())
+    return render(request,'index.html',{'response': response})
