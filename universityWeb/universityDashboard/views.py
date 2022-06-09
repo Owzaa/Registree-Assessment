e7e494cbbef12ccdefca3de4e6c63e6bb904d9eb
Owzaa <nyondo.olwethu@gmail.com>
2022-06-09 16:02:37 +0200

    commit on master

--- universityWeb/universityDashboard/views.py
+++ universityWeb/universityDashboard/views.py
@@ -1,20 +1,22 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-import requests
 import os
 from .services import get_universitiesMarks, get_universitiesNames
 
 def homeView(request):
     title = 'University - Dashboard'
-    return render(request,'dashboard.html',{'title':title})
+    universityData =get_universitiesNames() 
+    return render(request,'dashboard.html',{'universityData':universityData,'title':title})
 
 
-class GetUniversityNames(TemplateView):   
+class GetUniversityNames(TemplateView):
+    Template_name ='dashboard.html'   
     def get_context_data (self,*args, **kwargs):
         context= {'names':get_universitiesNames(),}  
         return context
 
 class GetUniversityMarks(TemplateView):
+    Template_name ='dashboard.html'   
     def get_context_data (self,*args, **kwargs):
         context= {'marks':get_universitiesMarks(),}  
         return context
