972fd9ccefef98caa6dfa43423936183b94575aa
Owzaa <nyondo.olwethu@gmail.com>
2022-06-09 15:50:36 +0200

    commit on master

--- universityWeb/universityDashboard/services.py
+++ universityWeb/universityDashboard/services.py
@@ -12,3 +12,16 @@ def get_universitiesNames():
     response = requests.get(url,headers={'Authorization': 'Bearer %s' % os.getenv('DO_ACCESS_TOKEN')})
     university = response.json()
     university_names = []
+
+
+def get_universitiesMarks():
+    url = "https://registree-coding-challenge.glitch.me/SU/marks"
+    response = requests.get(url,headers={'Authorization': 'Bearer %s' % os.getenv('DO_ACCESS_TOKEN')})
+    university = response.json()
+    university_marks = []
+
+def get_universitiesNames():
+    url = "https://registree-coding-challenge.glitch.me/SU/names"
+    response = requests.get(url,headers={'Authorization': 'Bearer %s' % os.getenv('DO_ACCESS_TOKEN')})
+    university = response.json()
+    university_names = []
