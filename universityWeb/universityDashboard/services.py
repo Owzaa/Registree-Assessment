de0882551c42bb954ebc58914c21ca01e3eb54ae
Owzaa <nyondo.olwethu@gmail.com>
2022-06-09 16:28:29 +0200

    commit on master

--- universityWeb/universityDashboard/services.py
+++ universityWeb/universityDashboard/services.py
@@ -4,14 +4,20 @@ import requests
 def get_universitiesMarks():
     url = "https://registree-coding-challenge.glitch.me/UJ/marks" 
     response = requests.get(url,headers={'Authorization': 'Bearer %s' % os.getenv('DO_ACCESS_TOKEN')})
-    universities = response.json()
+    universities  = response.json()
     university_marks = []
+    for i in range(len(universities['universities'])):
+        university_marks.append(universities['universities'][i])
+    return university_marks
 
 def get_universitiesNames():
     url = "https://registree-coding-challenge.glitch.me/UJ/names"
     response = requests.get(url,headers={'Authorization': 'Bearer %s' % os.getenv('DO_ACCESS_TOKEN')})
     universities = response.json()
     university_names = []
+    for i in range(len(universities['universities'])):
+        university_names.append(universities['universities'][i])
+    return university_names
 
 
 def get_universitiesMarks():
@@ -19,9 +25,16 @@ def get_universitiesMarks():
     response = requests.get(url,headers={'Authorization': 'Bearer %s' % os.getenv('DO_ACCESS_TOKEN')})
     universities = response.json()
     university_marks = []
+    for i in range(len(universities_marks['universities_marks'])):
+        university_marks.append(universities_marks['universities_marks'][i])
+    return university_marks
+
 
 def get_universitiesNames():
     url = "https://registree-coding-challenge.glitch.me/SU/names"
     response = requests.get(url,headers={'Authorization': 'Bearer %s' % os.getenv('DO_ACCESS_TOKEN')})
     universities = response.json()
     university_names = []
+    for i in range(len(universities['universities'])):
+        university_names.append(universities['universities'][i])
+    return university_names
